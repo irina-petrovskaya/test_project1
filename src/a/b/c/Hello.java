c39d422fd9f289d196dfbd668c7257dd923c9301
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2010-09-07 16:02:16 +0400

    add Hello.java

---
+++ src/a/b/c/Hello.java
@@ -0,0 +1,11 @@
+package a.b.c;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Irina.Petrovskaya
+ * Date: 07.09.2010
+ * Time: 16:01:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+}
