526c80cf9e13d477a7a2cd9c9d1215c3855b8a39
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2010-10-08 13:33:55 +0400

    errt

--- src/a/b/xxxxxx/Foo.java
+++ src/a/b/xxxxxx/Foo.java
@@ -1,7 +1,8 @@
-package a.b.c;
+package a.b.xxxxxx;
 
 /**
  * Created by  Irina.Petrovskaya at 07.10.10, 13:23
  */
 public class Foo {
+    // change  eeeeeeeee   dddd  eeeeeeeee   eeeeeeeeeeee
 }
