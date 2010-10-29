122474303f155da947e28d2bc429b88650c3e490
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2010-10-29 14:37:06 +0400

    change 1 in branch

--- src/a/b/xxxxxx/Foo.java
+++ src/a/b/xxxxxx/Foo.java
@@ -4,5 +4,5 @@ package a.b.xxxxxx;
  * Created by  Irina.Petrovskaya at 07.10.10, 13:23
  */
 public class Foo {
-    // change  eeeeeeeee   dddd  eeeeeeeee   eeeeeeeeeeee
+    // change in branch
 }
