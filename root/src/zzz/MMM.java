5667d347cd94d07063cd107c6d971a2678c6291d
Irina.Petrovskaya <Irina.Petrovskaya@unit-211.Labs.IntelliJ.Net>
2011-12-08 15:57:35 +0400

    accessors

--- root/src/zzz/MMM.java
+++ root/src/zzz/MMM.java
@@ -7,4 +7,20 @@ public class MMM {
     // jjjjjj
     String rrr;
     int mm;
+
+    public String getRrr() {
+        return rrr;
+    }
+
+    public void setRrr(String rrr) {
+        this.rrr = rrr;
+    }
+
+    public int getMm() {
+        return mm;
+    }
+
+    public void setMm(int mm) {
+        this.mm = mm;
+    }
 }
