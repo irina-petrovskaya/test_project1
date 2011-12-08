8669f14cb845d53a9bb8cce90b40463a764fc2ae
Irina.Petrovskaya <Irina.Petrovskaya@unit-211.Labs.IntelliJ.Net>
2011-12-08 16:57:56 +0400

    changed

--- src/a/b/rrrrr/R1.java
+++ src/a/b/rrrrr/R1.java
@@ -4,7 +4,7 @@ package a.b.rrrrr;
  * Created by  Irina.Petrovskaya at 08.10.10, 14:08
  */
 public class R1 {
-   String sss;   // jjjj
+   String sss;   // bb2
 
     public String getSss() {
         return sss;
