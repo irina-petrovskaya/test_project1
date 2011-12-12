3ed9dc290b844355e6bfaa3647efa6665963d6cc
irochka <me@foo.com>
2011-12-12 14:16:25 +0400

    add fffimpl

---
+++ root/src/xxxx/FFFImpl.java
@@ -0,0 +1,11 @@
+package xxxx;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Irina.Petrovskaya
+ * Date: 12.12.11
+ * Time: 14:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class FFFImpl extends FFF {
+}
