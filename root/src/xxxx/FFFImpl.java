6acdc954a52f5cbc3409c7822590b66bc35d6690
irochka <me@foo.com>
2011-12-12 14:58:50 +0400

    override zoo

--- root/src/xxxx/FFFImpl.java
+++ root/src/xxxx/FFFImpl.java
@@ -2,6 +2,8 @@ package xxxx;
 
   // yyyy
 public class FFFImpl extends FFF {
-
-    //
-}
+      @Override
+      public String zoo() {
+          return super.zoo();    //To change body of overridden methods use File | Settings | File Templates.
+      }
+  }
