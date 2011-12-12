49a78f85ec8b2b7c1d88ae82487d956203b56299
Irina.Petrovskaya <Irina.Petrovskaya@unit-211.Labs.IntelliJ.Net>
2011-12-12 17:10:54 +0400

    bbb2 again

--- root/src/zzz/OOO.java
+++ root/src/zzz/OOO.java
@@ -4,4 +4,6 @@ package zzz;
  * by Irina.Petrovskaya, on 08.12.11, 13:15
  */
 public class OOO {
+
+    // bb2
 }
--- src/a/b/xxxxxx/Foo.java
+++ src/a/b/xxxxxx/Foo.java
@@ -4,5 +4,5 @@ package a.b.xxxxxx;
  * Created by  Irina.Petrovskaya at 07.10.10, 13:23
  */
 public class Foo {
-    // change in branchvvvv
+    // change in branch bb2
 }
