de44e0b36b7cb3e27e7d2fa028d3bd25beeb3ecc
Irina.Petrovskaya <Irina.Petrovskaya@unit-211.Labs.IntelliJ.Net>
2011-12-12 16:02:43 +0400

    override methods

--- root/src/xxxx/FFFImpl.java
+++ root/src/xxxx/FFFImpl.java
@@ -2,4 +2,14 @@ package xxxx;
 
   // yyyy
 public class FFFImpl extends FFF {
-}
+
+      @java.lang.Override
+      public void foo() {
+          super.foo();    //To change body of overridden methods use File | Settings | File Templates.
+      }
+
+      @java.lang.Override
+      public void boo() {
+          super.boo();    //To change body of overridden methods use File | Settings | File Templates.
+      }
+  }
