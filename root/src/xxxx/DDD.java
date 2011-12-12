e1dde1f30b585a0b250a612b95641050d40c04df
irochka <me@foo.com>
2011-12-12 16:07:48 +0400

    edit DDD

--- root/src/xxxx/DDD.java
+++ root/src/xxxx/DDD.java
@@ -1,11 +1,5 @@
 package xxxx;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Irina.Petrovskaya
- * Date: 12.12.11
- * Time: 16:07
- * To change this template use File | Settings | File Templates.
- */
+//
 public interface DDD {
 }
