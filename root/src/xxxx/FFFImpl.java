20e4b2be9e2a349dc9b3280c3a0cd3d589b8dc8c
Irina.Petrovskaya <Irina.Petrovskaya@unit-211.Labs.IntelliJ.Net>
2011-12-12 16:06:48 +0400

    edit message bodies

--- root/src/xxxx/FFFImpl.java
+++ root/src/xxxx/FFFImpl.java
@@ -15,6 +15,6 @@ public class FFFImpl extends FFF {
 
       @Override
       public String zoo() {
-          return super.zoo();    //To change body of overridden methods use File | Settings | File Templates.
+          return super.zoo();
       }
   }
