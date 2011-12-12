9b58305d410c3a1ea3280c3500c8269374adabf6
irochka <me@foo.com>
2011-12-12 16:11:00 +0400

    implementation

--- root/src/xxxx/FFF.java
+++ root/src/xxxx/FFF.java
@@ -3,7 +3,7 @@ package xxxx;
 /**
  * by Irina.Petrovskaya, on 05.12.11, 19:16
  */
-public class FFF {
+public class FFF implements DDD {
 
     public void foo(){
 
@@ -14,5 +14,10 @@ public class FFF {
     }
     public String zoo(){
         return "";
+    }
+
+    @Override
+    public void aaa() {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
