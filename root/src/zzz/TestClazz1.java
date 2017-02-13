1333d984742296090baed97175bc9e8958316b88
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-02-13 17:55:57 +0300

    add TestClazz1.java

---
+++ root/src/zzz/TestClazz1.java
@@ -0,0 +1,11 @@
+package zzz;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 2/13/2017.
+ * Project: test_project1
+ * test
+ * *******************************
+ */
+public class TestClazz1 {
+}
